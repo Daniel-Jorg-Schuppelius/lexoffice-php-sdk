b890d8e6977a12c2b3c4bdcabc9ba25f0402a145
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2024-07-23 15:12:34 +0200

    Change Property CountryCode to Enum

--- src/Entities/Countries/Countries.php
+++ src/Entities/Countries/Countries.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Lexoffice\Entities\Countries;
 
 use Lexoffice\Contracts\Abstracts\NamedEntity;
+use Lexoffice\Enums\CountryCode;
 use Lexoffice\Enums\TaxClassification;
 
 class Countries extends NamedEntity {
-    public string $countryCode;
+    public CountryCode $countryCode;
     public string $countryNameEN;
     public string $countryNameDE;
     public TaxClassification $taxClassification;
