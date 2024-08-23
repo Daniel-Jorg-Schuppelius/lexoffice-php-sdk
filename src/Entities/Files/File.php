aebd4998b8f351ed425f6f125fd79cd250082326
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2024-08-23 14:09:29 +0200

    refine filevalidation

--- src/Entities/Files/File.php
+++ src/Entities/Files/File.php
@@ -9,6 +9,8 @@ use Lexoffice\Contracts\Interfaces\IdentifiableInterface;
 use Psr\Log\LoggerInterface;
 
 class File extends NamedEntity implements IdentifiableInterface {
+    public const ALLOWED_EXTENSIONS = ['pdf', 'jpg', 'png'];
+
     protected ?FileID $id;
     protected ?string $filePath;
 
@@ -24,7 +26,20 @@ class File extends NamedEntity implements IdentifiableInterface {
         return $this->filePath;
     }
 
+    public function getFileSize(): int {
+        return filesize($this->filePath);
+    }
+
     public function isValid(): bool {
-        return !is_null($this->id) || !is_null($this->filePath);
+        if (isset($this->filePath) && file_exists($this->filePath)) {
+            $extension = strtolower(pathinfo($this->filePath, PATHINFO_EXTENSION));
+
+            if (in_array($extension, self::ALLOWED_EXTENSIONS)) {
+                if ($this->getFileSize() <= 5000000) {
+                    return true;
+                }
+            }
+        }
+        return isset($this->id);
     }
 }
