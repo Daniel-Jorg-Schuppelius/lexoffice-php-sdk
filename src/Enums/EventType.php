df753c83143be96ff2857d7700c36edd3edd5e1a
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2024-10-06 13:03:49 +0200

    add filecomments

--- src/Enums/EventType.php
+++ src/Enums/EventType.php
@@ -1,11 +1,18 @@
 <?php
+/*
+ * Created on   : Sun Oct 06 2024
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : EventType.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
 
 declare(strict_types=1);
 
 namespace Lexoffice\Enums;
 
-enum EventType: string
-{
+enum EventType: string {
     case ARTICLE_CREATED = 'article.created';
     case ARTICLE_CHANGED = 'article.changed';
     case ARTICLE_DELETED = 'article.deleted';
