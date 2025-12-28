76b0cef371474e896fc31853f3f8823d3f475a44
DSchuppelius <daniel.schuppelius@gmail.com>
2025-12-28 14:33:51 +0100

    fix lists for email and phone

---
+++ src/Entities/Contacts/EmailAddressList.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Created on   : Sat Dec 28 2024
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : EmailAddressList.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+declare(strict_types=1);
+
+namespace Lexoffice\Entities\Contacts;
+
+use APIToolkit\Entities\StringValues;
+
+class EmailAddressList extends StringValues {
+}
---
+++ src/Entities/Contacts/PhoneNumberList.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Created on   : Sat Dec 28 2024
+ * Author       : Daniel Jörg Schuppelius
+ * Author Uri   : https://schuppelius.org
+ * Filename     : PhoneNumberList.php
+ * License      : MIT License
+ * License Uri  : https://opensource.org/license/mit
+ */
+
+declare(strict_types=1);
+
+namespace Lexoffice\Entities\Contacts;
+
+use APIToolkit\Entities\StringValues;
+
+class PhoneNumberList extends StringValues {
+}
