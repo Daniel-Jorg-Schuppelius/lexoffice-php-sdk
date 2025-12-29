e26204d2f233d7f4fdb333868e3b2ceddd66e1b8
DSchuppelius <daniel.schuppelius@gmail.com>
2025-12-29 10:16:01 +0100

    update auth

--- src/API/Client.php
+++ src/API/Client.php
@@ -12,10 +12,10 @@ declare(strict_types=1);
 
 namespace Lexoffice\API;
 
+use APIToolkit\Contracts\Abstracts\API\Authentication\BearerAuthentication;
 use APIToolkit\Contracts\Abstracts\API\ClientAbstract;
 use Psr\Log\LoggerInterface;
 use GuzzleHttp\Client as HttpClient;
-use Psr\Http\Message\ResponseInterface;
 
 class Client extends ClientAbstract {
     public const MIN_INTERVAL = 0.5;
@@ -26,14 +26,13 @@ class Client extends ClientAbstract {
             'base_uri' => $baseUrl,
             'timeout' => 2.0,
             'headers' => [
-                'Authorization' => 'Bearer ' . $apiKey,
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
             ],
         ]), $logger, $sleepAfterRequest);
-    }
 
-    protected function requestWithRetry(string $method, string $uri, array $options = [], int $maxRetries = 3, int $retryDelay = 1): ResponseInterface {
-        return parent::requestWithRetry($method, $uri, $options, $maxRetries, $retryDelay);
+        if ($apiKey !== null) {
+            $this->setAuthentication(new BearerAuthentication($apiKey));
+        }
     }
 }
